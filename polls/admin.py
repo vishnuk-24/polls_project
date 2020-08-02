41741cc8446c587565e0c2412c41495e46a3f0dd
VishnuK <vishnuk@labglo.com>
2020-08-02 12:55:40 +0530

    Modifies admin interface.

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,25 @@ from django.contrib import admin
 
 from .models import Choice, Question, Article, Reporter
 
-admin.site.register(Choice)
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date',)
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 10
+    date_hierarchy = 'pub_date'
+
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Article)
 admin.site.register(Reporter)
--- polls_project/urls.py
+++ polls_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Polls Administration"
+admin.site.site_title = "Polls Admin Portal"
+admin.site.index_title = "Welcome to Polls Administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
